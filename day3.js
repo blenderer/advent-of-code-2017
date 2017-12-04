5f2abf227eb86a9826f165ce937cf0dd8b71bf00
Eric Harrison <eric.h@rrison.io>
2017-12-03 22:19:48 -0500

    day 3: solution 2

--- day3.js
+++ day3.js
@@ -82,3 +82,85 @@ while (foundNumber !== 1) {
 console.log(steps);
 
 // solution 2:
+
+function getNeigborsSum(grid, x, y) {
+	let neigbors = [
+		grid[x + 1][y], //right
+		grid[x + 1][y - 1], //rightup
+		grid[x][y - 1], // up
+		grid[x - 1][y - 1], //upleft
+		grid[x - 1][y], // left
+		grid[x - 1][y + 1], //leftdown
+		grid[x][y + 1], // down
+		grid[x + 1][y + 1], // downright
+	];
+
+	return neigbors.reduce((sum, neigbor) => {
+		if (neigbor) return sum + neigbor;
+
+		return sum;
+    }, 0);
+}
+
+highest = 0;
+
+function buildDiagonalGridAndHash (max) {
+
+    let grid = [[1]];
+    let hash = [[0, 0]];
+    let current = 1;
+
+    while (current <= max) {
+        let last = hash[hash.length - 1];
+        let lastX = last[0];
+        let lastY = last[1];
+
+        if (!grid[lastX - 1]) {
+            grid[lastX - 1] = [];
+        }
+        if (!grid[lastX + 1]) {
+            grid[lastX + 1] = [];
+        }
+		if (!grid[lastX - 2]) {
+            grid[lastX - 2] = [];
+        }
+        if (!grid[lastX + 2]) {
+            grid[lastX + 2] = [];
+        }
+        let right = grid[lastX + 1][lastY];
+        let up = grid[lastX][lastY - 1];
+        let left = grid[lastX - 1][lastY];
+        let down = grid[lastX][lastY + 1];
+
+        if (!left && !right && !down && !up) {
+            hash.push([lastX + 1, lastY]);
+			current = getNeigborsSum(grid, lastX + 1, lastY);
+            grid[lastX + 1][lastY] = current;
+        } else if (left && !up) {
+            hash.push([lastX, lastY - 1]);
+			current = getNeigborsSum(grid, lastX, lastY - 1);
+            grid[lastX][lastY - 1] = current;
+        } else if (down && !left) {
+            hash.push([lastX - 1, lastY]);
+			current = getNeigborsSum(grid, lastX - 1, lastY);
+            grid[lastX - 1][lastY] = current;
+        } else if (right && !down) {
+            hash.push([lastX, lastY + 1]);
+			current = getNeigborsSum(grid, lastX, lastY + 1);
+            grid[lastX][lastY + 1] = current;
+        } else if (!right && up) {
+            hash.push([lastX + 1, lastY]);
+			current = getNeigborsSum(grid, lastX + 1, lastY);
+            grid[lastX + 1][lastY] = current;
+        }
+
+		if (current > highest) {
+			highest = current;
+        }
+
+    }
+	return {grid, hash};
+}
+
+buildDiagonalGridAndHash(747);
+console.log(highest);
